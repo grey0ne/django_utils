fb9ce28d15dd839ba29de16e416ce51c5b1d7fde
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-06-04 16:11:38 +0300

    base64 file decode refactoring

--- helpers.py
+++ helpers.py
@@ -3,8 +3,14 @@ from django.core.files.base import ContentFile
 
 
 def base64_to_file(data: str, name: str) -> ContentFile:
-    format, imgstr = data.split(';base64,') 
-    ext = format.split('/')[-1] 
+    splitted = data.split(';base64,')
+    if len(splitted) == 2:
+        format, imgstr = splitted
+        ext = format.split('/')[-1]
+        name = name + '.' + ext
+    else:
+        imgstr = data
 
-    return ContentFile(base64.b64decode(imgstr), name=name + '.' + ext)
+
+    return ContentFile(base64.b64decode(imgstr), name=name)
 
