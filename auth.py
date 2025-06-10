fdba5036f305740db2d8a9d825969ac7995851c2
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-06-10 11:28:45 +0300

    fix async auth backend

--- auth.py
+++ auth.py
@@ -6,10 +6,17 @@ from dataorm.schema import AuthData
 from users.models import User
 from typing import Any
 
+class UserNotAuthenticatedError(Exception):
+    """Custom exception for unauthenticated user access."""
+    pass
+
+
 async def async_get_user(request: HttpRequest) -> User:
     user_pk = User._meta.pk
     if user_pk is None:
         raise ValueError("User model does not have a primary key defined.")
+    if SESSION_KEY not in request.session:
+        raise UserNotAuthenticatedError("User is not authenticated.")
     user_id = user_pk.to_python(request.session[SESSION_KEY])
     return await User.objects.aget(pk=user_id)
 
@@ -22,11 +29,11 @@ class AsyncSessionAuth(APIKeyCookie):
 
 
     async def authenticate(self, request: HttpRequest, key: str | None) -> Any:
-        user = await async_get_user(request)
-        if user.is_authenticated:
-            return await self.get_auth_data(request_user=user, request=request)
-
-        return None
+        try:
+            user = await async_get_user(request)
+        except UserNotAuthenticatedError:
+            return None
+        return await self.get_auth_data(request_user=user, request=request)
 
 
 django_auth = AsyncSessionAuth()
